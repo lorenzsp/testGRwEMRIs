788391a5c511d2307dc5fbb487def12d281b203d
NAME NAME <EMAIL@DOMAIN.TLD>
2023-07-22 11:54:22 +0200

    scott Jacobian match Schw, and Susanna ev

--- check_Susanna/traj_produce.py
+++ check_Susanna/traj_produce.py
@@ -32,25 +32,17 @@ traj = EMRIInspiral(func="KerrEccentricEquatorial")
 
 # Susanna trajectory
 charge = 0.0
-t_S, p_S, e_S, F1, F2, Om1, Om2, PhiphiS, PhirS = np.loadtxt("evolution_GR_a09_rp6_ra11_newmsun.dat").T
 
-a=0.9
+
 M=1e6
 mu=1e1
-p0, e0 = p_S[0], e_S[0]
-# run trajectory
-t, p, e, x, Phi_phi, Phi_theta, Phi_r = traj(M, mu, a, p0, e0, 1.0, charge, T=1.0)
+T=4.0
+for ii in range(5):
+    p0, e0 = np.random.uniform(8.0,15.0), np.random.uniform(0.01, 0.5)
+    a=np.random.uniform(0.0,0.99)
+    # run trajectory
+    t, p, e, x, Phi_phi, Phi_theta, Phi_r = traj(M, mu, a, p0, e0, 1.0, charge, T=T)
 
-print('run',p0,e0,a)
+    out = np.asarray((t, p, e, x, Phi_phi, Phi_theta, Phi_r)).T
 
-interp = CubicSplineInterpolant(t, Phi_phi)
-interp2 = CubicSplineInterpolant(t, Phi_r)
-
-plt.figure()
-plt.title(f"a={a},M={M:.1e},mu={mu:.1e}\n e0={e0:.2}, p0={p0:.2}, sigma={charge:.2e}")
-plt.semilogy(t_S, np.abs(interp(t_S) - PhiphiS),'-',label=f"phi")
-plt.semilogy(t_S, np.abs(interp2(t_S) - PhirS),'-',label=f"r")
-plt.xlabel('t [s]')
-plt.ylabel('Phase difference')
-plt.legend()
-plt.savefig('Phase_difference')
+    np.savetxt(f"output_traj_M{M}_mu{mu}_a{a}_p{p0}_e{e0}_charge{charge}_T{T}yrs",out,header="t p e x Phi_phi Phi_theta Phi_r")
