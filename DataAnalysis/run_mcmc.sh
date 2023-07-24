9d458c1c14df4873aa9da0c9c1d3c8641ef04795
Lorenzo Speri <lorenzo.speri@gmail.com>
2023-07-25 00:08:53 +0200

    new runs started

---
+++ DataAnalysis/run_mcmc.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Assign variables to each parameter
+Tobs=2
+dt=15.0
+M=1e6
+mu=1e1
+a=0.9
+p0=13.0
+e0=0.2
+x0=1.0
+charge=0.0
+dev=3
+nwalkers=32
+ntemps=1
+nsteps=500000
+
+# Create a dynamic output filename
+output_filename="output_Tobs${Tobs}_dt${dt}_M${M}_mu${mu}_a${a}_p0${p0}_e0${e0}_x0${x0}_charge${charge}_dev${dev}_nwalkers${nwalkers}_ntemps${ntemps}_nsteps${nsteps}.txt"
+
+# Execute the Python command and redirect output to the dynamic filename
+nohup python mcmc.py -Tobs $Tobs -dt $dt -M $M -mu $mu -a $a -p0 $p0 -e0 $e0 -x0 $x0 -charge $charge -dev $dev -nwalkers $nwalkers -ntemps $ntemps -nsteps $nsteps > $output_filename &
