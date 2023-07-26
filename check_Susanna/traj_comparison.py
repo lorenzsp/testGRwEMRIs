55c764a905f123c2dabb4fb7fe2092e8e05ffac7
NAME NAME <EMAIL@DOMAIN.TLD>
2023-07-26 13:18:14 +0200

    comparison with Susi

--- check_Susanna/traj_comparison.py
+++ check_Susanna/traj_comparison.py
@@ -30,21 +30,28 @@ print(os.getpid())
 # initialize trajectory class
 traj = EMRIInspiral(func="KerrEccentricEquatorial")
 
+filename = "evolution_GR_aneg09876_rp9_ra15"#"evolution_GR_a01234_rp6_ra11"
 # Susanna trajectory
 charge = 0.0
-t_S, p_S, e_S, F1, F2, Om1, Om2, PhiphiS, PhirS = np.loadtxt("evolution_GR_a09_rp6_ra11_newmsun.dat").T
+t_S, p_S, e_S, F1, F2, Om1, Om2, PhiphiS, PhirS = np.loadtxt(filename + ".dat").T
 
-a=0.9
+a=0.9876
 M=1e6
 mu=1e1
 p0, e0 = p_S[0], e_S[0]
 # run trajectory
-t, p, e, x, Phi_phi, Phi_theta, Phi_r = traj(M, mu, a, p0, e0, 1.0, charge, T=1.5)
+t, p, e, x, Phi_phi, Phi_theta, Phi_r = traj(M, mu, a, p0, e0, -1.0, charge, T=4.0)
 
 print('run',p0,e0,a)
 
 interp = CubicSplineInterpolant(t, Phi_phi)
 interp2 = CubicSplineInterpolant(t, Phi_r)
+last_p = 5
+t_S = t_S[:-last_p]
+PhiphiS = PhiphiS[:-last_p]
+PhirS = PhirS[:-last_p]
+p_S = p_S[:-last_p]
+e_S = e_S[:-last_p]
 
 plt.figure()
 plt.title(f"a={a},M={M:.1e},mu={mu:.1e}\n e0={e0:.2}, p0={p0:.2}, sigma={charge:.2e}")
@@ -53,4 +60,26 @@ plt.semilogy(t_S, np.abs(interp2(t_S) - PhirS),'-',label=f"r")
 plt.xlabel('t [s]')
 plt.ylabel('Phase difference')
 plt.legend()
-plt.savefig('Phase_difference')
+plt.savefig('Phase_difference_'+filename)
+
+interp = CubicSplineInterpolant(t, p)
+interp2 = CubicSplineInterpolant(t, e)
+last_p = 5
+
+plt.figure()
+plt.title(f"a={a},M={M:.1e},mu={mu:.1e}\n e0={e0:.2}, p0={p0:.2}, sigma={charge:.2e}")
+plt.semilogy(t_S, np.abs(interp(t_S) - p_S),'-',label=f"p")
+plt.semilogy(t_S, np.abs(interp2(t_S) - e_S),'-',label=f"e")
+plt.xlabel('t [s]')
+plt.ylabel('orbital difference')
+plt.legend()
+plt.savefig('p_e_difference_'+filename)
+
+plt.figure()
+plt.title(f"a={a},M={M:.1e},mu={mu:.1e}\n e0={e0:.2}, p0={p0:.2}, sigma={charge:.2e}")
+plt.semilogy(p_S, e_S,'-',label=f"S")
+plt.semilogy(p, e,'--',label=f"FEW")
+plt.xlabel('p')
+plt.ylabel('e')
+plt.legend()
+plt.savefig('p_e_plane_'+filename)
