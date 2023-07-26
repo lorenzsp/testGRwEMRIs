d830f4ed4fce05ad23e90d04acafc26bb18e8683
Lorenzo Speri <lorenzo.speri@gmail.com>
2023-07-26 15:28:32 +0200

    new mcmc runs

---
+++ DataAnalysis/lakshmi_mcmc.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Assign variables to each parameter
+Tobs=2
+dt=15.0
+M=1e6
+mu=1e1
+a=0.9
+p0=13.0
+e0=0.4
+x0=1.0
+charge=0.0
+nwalkers=32
+ntemps=1
+nsteps=500000
+
+# ---------------------------------------------------------
+# dev=6
+
+# Tobs=4
+
+# # Create a dynamic output filename
+# output_filename="output_Tobs${Tobs}_dt${dt}_M${M}_mu${mu}_a${a}_p0${p0}_e0${e0}_x0${x0}_charge${charge}_dev${dev}_nwalkers${nwalkers}_ntemps${ntemps}_nsteps${nsteps}.txt"
+
+# # Execute the Python command and redirect output to the dynamic filename
+# nohup python mcmc.py -Tobs $Tobs -dt $dt -M $M -mu $mu -a $a -p0 $p0 -e0 $e0 -x0 $x0 -charge $charge -dev $dev -nwalkers $nwalkers -ntemps $ntemps -nsteps $nsteps > $output_filename &
+# ---------------------------------------------------------
+# dev=7
+
+# Tobs=2
+# M=5e5
+# mu=5.0
+# # Create a dynamic output filename
+# output_filename="output_Tobs${Tobs}_dt${dt}_M${M}_mu${mu}_a${a}_p0${p0}_e0${e0}_x0${x0}_charge${charge}_dev${dev}_nwalkers${nwalkers}_ntemps${ntemps}_nsteps${nsteps}.txt"
+
+# # Execute the Python command and redirect output to the dynamic filename
+# nohup python mcmc.py -Tobs $Tobs -dt $dt -M $M -mu $mu -a $a -p0 $p0 -e0 $e0 -x0 $x0 -charge $charge -dev $dev -nwalkers $nwalkers -ntemps $ntemps -nsteps $nsteps > $output_filename &
+# # ---------------------------------------------------------
+dev=5
+# Create a dynamic output filename
+output_filename="output_Tobs${Tobs}_dt${dt}_M${M}_mu${mu}_a${a}_p0${p0}_e0${e0}_x0${x0}_charge${charge}_dev${dev}_nwalkers${nwalkers}_ntemps${ntemps}_nsteps${nsteps}.txt"
+
+# Execute the Python command and redirect output to the dynamic filename
+nohup python mcmc.py -Tobs $Tobs -dt $dt -M $M -mu $mu -a $a -p0 $p0 -e0 $e0 -x0 $x0 -charge $charge -dev $dev -nwalkers $nwalkers -ntemps $ntemps -nsteps $nsteps > $output_filename &
+
