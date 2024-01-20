a76b380e85779761f55fccd05c5fbf73d19964dd
Lorenzo Speri <lorenzo.speri@gmail.com>
2024-01-20 17:10:54 +0100

    new mcmc based on prkins

--- DataAnalysis/run_mcmc.sh
+++ DataAnalysis/run_mcmc.sh
@@ -42,15 +42,15 @@ output_filename="output_Tobs${Tobs}_dt${dt}_M${M}_mu${mu}_a${a}_p0${p0}_e0${e0}_
 nohup python mcmc.py -Tobs $Tobs -dt $dt -M $M -mu $mu -a $a -p0 $p0 -e0 $e0 -x0 $x0 -charge $charge -dev $dev -nwalkers $nwalkers -ntemps $ntemps -nsteps $nsteps > $output_filename &
 
 e0=0.4
----------------------------------------------------------
-dev=3
-Tobs=4
+# ---------------------------------------------------------
+# dev=3
+# Tobs=4
 
-# Create a dynamic output filename
-output_filename="output_Tobs${Tobs}_dt${dt}_M${M}_mu${mu}_a${a}_p0${p0}_e0${e0}_x0${x0}_charge${charge}_dev${dev}_nwalkers${nwalkers}_ntemps${ntemps}_nsteps${nsteps}.txt"
+# # Create a dynamic output filename
+# output_filename="output_Tobs${Tobs}_dt${dt}_M${M}_mu${mu}_a${a}_p0${p0}_e0${e0}_x0${x0}_charge${charge}_dev${dev}_nwalkers${nwalkers}_ntemps${ntemps}_nsteps${nsteps}.txt"
 
-# Execute the Python command and redirect output to the dynamic filename
-nohup python mcmc.py -Tobs $Tobs -dt $dt -M $M -mu $mu -a $a -p0 $p0 -e0 $e0 -x0 $x0 -charge $charge -dev $dev -nwalkers $nwalkers -ntemps $ntemps -nsteps $nsteps > $output_filename &
+# # Execute the Python command and redirect output to the dynamic filename
+# nohup python mcmc.py -Tobs $Tobs -dt $dt -M $M -mu $mu -a $a -p0 $p0 -e0 $e0 -x0 $x0 -charge $charge -dev $dev -nwalkers $nwalkers -ntemps $ntemps -nsteps $nsteps > $output_filename &
 
 # ---------------------------------------------------------
 dev=4
@@ -98,7 +98,7 @@ nohup python mcmc.py -Tobs $Tobs -dt $dt -M $M -mu $mu -a $a -p0 $p0 -e0 $e0 -x0
 # ---------------------------------------------------------
 M=5e5
 mu=5
-dev=0
+dev=3
 charge=1e-2
 # Create a dynamic output filename
 output_filename="output_Tobs${Tobs}_dt${dt}_M${M}_mu${mu}_a${a}_p0${p0}_e0${e0}_x0${x0}_charge${charge}_dev${dev}_nwalkers${nwalkers}_ntemps${ntemps}_nsteps${nsteps}.txt"
