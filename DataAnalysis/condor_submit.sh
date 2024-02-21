9ef01d17866621d2a46eaf5597246124eb461706
Lorenzo Speri <lorenzo.speri@gmail.com>
2024-02-21 09:27:23 +0100

    submission with condor

---
+++ DataAnalysis/condor_submit.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+# chmod +x mcmc.py
+
+# Assign variables to each parameter
+Tobs=2
+dt=10.0
+M=1e6
+mu=1e1
+a=0.95
+p0=13.0
+e0=0.4
+x0=1.0
+charge=0.0
+nwalkers=16
+ntemps=3
+nsteps=500000
+
+# ---------------------------------------------------------
+dev=3
+# Create a dynamic output filename
+output_filename="output_intr_Tobs${Tobs}_dt${dt}_M${M}_mu${mu}_a${a}_p0${p0}_e0${e0}_x0${x0}_charge${charge}_dev${dev}_nwalkers${nwalkers}_ntemps${ntemps}_nsteps${nsteps}.txt"
+
+# Execute the Python command and redirect output to the dynamic filename
+condor_submit -a "Tobs=$Tobs" -a "dt=$dt" -a "M=$M" -a "mu=$mu" -a "a=$a" -a "p0=$p0" -a "e0=$e0" -a "x0=$x0" -a "charge=$charge" -a "nwalkers=$nwalkers" -a "ntemps=$ntemps" -a "nsteps=$nsteps" -a "dev=$dev" -a "output_filename=$output_filename" submit_file.submit
+
+# ---------------------------------------------------------
+dev=4
+a=0.80
+# Create a dynamic output filename
+output_filename="output_intr_Tobs${Tobs}_dt${dt}_M${M}_mu${mu}_a${a}_p0${p0}_e0${e0}_x0${x0}_charge${charge}_dev${dev}_nwalkers${nwalkers}_ntemps${ntemps}_nsteps${nsteps}.txt"
+
+# Execute the Python command and redirect output to the dynamic filename
+condor_submit -a "Tobs=$Tobs" -a "dt=$dt" -a "M=$M" -a "mu=$mu" -a "a=$a" -a "p0=$p0" -a "e0=$e0" -a "x0=$x0" -a "charge=$charge" -a "nwalkers=$nwalkers" -a "ntemps=$ntemps" -a "nsteps=$nsteps" -a "dev=$dev" -a "output_filename=$output_filename" submit_file.submit
+
+a=0.95
+# ---------------------------------------------------------
+dev=5
+e0=0.2
+# Create a dynamic output filename
+output_filename="output_intr_Tobs${Tobs}_dt${dt}_M${M}_mu${mu}_a${a}_p0${p0}_e0${e0}_x0${x0}_charge${charge}_dev${dev}_nwalkers${nwalkers}_ntemps${ntemps}_nsteps${nsteps}.txt"
+
+# Execute the Python command and redirect output to the dynamic filename
+condor_submit -a "Tobs=$Tobs" -a "dt=$dt" -a "M=$M" -a "mu=$mu" -a "a=$a" -a "p0=$p0" -a "e0=$e0" -a "x0=$x0" -a "charge=$charge" -a "nwalkers=$nwalkers" -a "ntemps=$ntemps" -a "nsteps=$nsteps" -a "dev=$dev" -a "output_filename=$output_filename" submit_file.submit
+
+e0=0.4
+
+# # ---------------------------------------------------------
+# dt=8.0
+dev=6
+Tobs=2
+M=7e5
+# Create a dynamic output filename
+output_filename="output_intr_Tobs${Tobs}_dt${dt}_M${M}_mu${mu}_a${a}_p0${p0}_e0${e0}_x0${x0}_charge${charge}_dev${dev}_nwalkers${nwalkers}_ntemps${ntemps}_nsteps${nsteps}.txt"
+
+# Execute the Python command and redirect output to the dynamic filename
+condor_submit -a "Tobs=$Tobs" -a "dt=$dt" -a "M=$M" -a "mu=$mu" -a "a=$a" -a "p0=$p0" -a "e0=$e0" -a "x0=$x0" -a "charge=$charge" -a "nwalkers=$nwalkers" -a "ntemps=$ntemps" -a "nsteps=$nsteps" -a "dev=$dev" -a "output_filename=$output_filename" submit_file.submit
+
+# ---------------------------------------------------------
+dev=7
+Tobs=2
+M=1e6
+mu=5
+# Create a dynamic output filename
+output_filename="output_intr_Tobs${Tobs}_dt${dt}_M${M}_mu${mu}_a${a}_p0${p0}_e0${e0}_x0${x0}_charge${charge}_dev${dev}_nwalkers${nwalkers}_ntemps${ntemps}_nsteps${nsteps}.txt"
+
+# Execute the Python command and redirect output to the dynamic filename
+condor_submit -a "Tobs=$Tobs" -a "dt=$dt" -a "M=$M" -a "mu=$mu" -a "a=$a" -a "p0=$p0" -a "e0=$e0" -a "x0=$x0" -a "charge=$charge" -a "nwalkers=$nwalkers" -a "ntemps=$ntemps" -a "nsteps=$nsteps" -a "dev=$dev" -a "output_filename=$output_filename" submit_file.submit
