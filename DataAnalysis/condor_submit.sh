cf7108eead50df24003c6b09f39a91a35e0d9951
Lorenzo Speri <lorenzo.speri@gmail.com>
2024-03-27 15:32:05 +0100

    new code

--- DataAnalysis/condor_submit.sh
+++ DataAnalysis/condor_submit.sh
@@ -3,7 +3,7 @@
 # then: bash condor_submit.sh
 
 # Assign variables to each parameter
-Tobs=2
+Tobs=2.0
 dt=10.0
 M=1e6
 mu=1e1
@@ -17,51 +17,49 @@ ntemps=1
 nsteps=500000
 
 # ---------------------------------------------------------
-dev=7
-# Create a dynamic output filename
-output_filename="output_lakshmi_Tobs${Tobs}_dt${dt}_M${M}_mu${mu}_a${a}_p0${p0}_e0${e0}_x0${x0}_charge${charge}_dev${dev}_nwalkers${nwalkers}_ntemps${ntemps}_nsteps${nsteps}.txt"
-
-# Execute the Python command and redirect output to the dynamic filename
-condor_submit -a "Tobs=$Tobs" -a "dt=$dt" -a "M=$M" -a "mu=$mu" -a "a=$a" -a "p0=$p0" -a "e0=$e0" -a "x0=$x0" -a "charge=$charge" -a "nwalkers=$nwalkers" -a "ntemps=$ntemps" -a "nsteps=$nsteps" -a "dev=$dev" -a "output_filename=$output_filename" submit_file.submit
-
-# ---------------------------------------------------------
-dev=3
-a=0.80
-# Create a dynamic output filename
-output_filename="output_lakshmi_Tobs${Tobs}_dt${dt}_M${M}_mu${mu}_a${a}_p0${p0}_e0${e0}_x0${x0}_charge${charge}_dev${dev}_nwalkers${nwalkers}_ntemps${ntemps}_nsteps${nsteps}.txt"
-
-# Execute the Python command and redirect output to the dynamic filename
-condor_submit -a "Tobs=$Tobs" -a "dt=$dt" -a "M=$M" -a "mu=$mu" -a "a=$a" -a "p0=$p0" -a "e0=$e0" -a "x0=$x0" -a "charge=$charge" -a "nwalkers=$nwalkers" -a "ntemps=$ntemps" -a "nsteps=$nsteps" -a "dev=$dev" -a "output_filename=$output_filename" submit_file.submit
-
-a=0.95
-# ---------------------------------------------------------
-dev=4
-e0=0.2
-# Create a dynamic output filename
-output_filename="output_lakshmi_Tobs${Tobs}_dt${dt}_M${M}_mu${mu}_a${a}_p0${p0}_e0${e0}_x0${x0}_charge${charge}_dev${dev}_nwalkers${nwalkers}_ntemps${ntemps}_nsteps${nsteps}.txt"
-
-# Execute the Python command and redirect output to the dynamic filename
-condor_submit -a "Tobs=$Tobs" -a "dt=$dt" -a "M=$M" -a "mu=$mu" -a "a=$a" -a "p0=$p0" -a "e0=$e0" -a "x0=$x0" -a "charge=$charge" -a "nwalkers=$nwalkers" -a "ntemps=$ntemps" -a "nsteps=$nsteps" -a "dev=$dev" -a "output_filename=$output_filename" submit_file.submit
-
-e0=0.4
-
-# ---------------------------------------------------------
 dev=5
-Tobs=2
-M=7e5
 # Create a dynamic output filename
 output_filename="output_lakshmi_Tobs${Tobs}_dt${dt}_M${M}_mu${mu}_a${a}_p0${p0}_e0${e0}_x0${x0}_charge${charge}_dev${dev}_nwalkers${nwalkers}_ntemps${ntemps}_nsteps${nsteps}.txt"
 
 # Execute the Python command and redirect output to the dynamic filename
 condor_submit -a "Tobs=$Tobs" -a "dt=$dt" -a "M=$M" -a "mu=$mu" -a "a=$a" -a "p0=$p0" -a "e0=$e0" -a "x0=$x0" -a "charge=$charge" -a "nwalkers=$nwalkers" -a "ntemps=$ntemps" -a "nsteps=$nsteps" -a "dev=$dev" -a "output_filename=$output_filename" submit_file.submit
 
-# ---------------------------------------------------------
-dev=6
-Tobs=2
-M=1e6
-mu=5
-# Create a dynamic output filename
-output_filename="output_lakshmi_Tobs${Tobs}_dt${dt}_M${M}_mu${mu}_a${a}_p0${p0}_e0${e0}_x0${x0}_charge${charge}_dev${dev}_nwalkers${nwalkers}_ntemps${ntemps}_nsteps${nsteps}.txt"
+# # ---------------------------------------------------------
+# dev=1
+# a=0.80
+# # Create a dynamic output filename
+# output_filename="output_lakshmi_Tobs${Tobs}_dt${dt}_M${M}_mu${mu}_a${a}_p0${p0}_e0${e0}_x0${x0}_charge${charge}_dev${dev}_nwalkers${nwalkers}_ntemps${ntemps}_nsteps${nsteps}.txt"
 
-# Execute the Python command and redirect output to the dynamic filename
-condor_submit -a "Tobs=$Tobs" -a "dt=$dt" -a "M=$M" -a "mu=$mu" -a "a=$a" -a "p0=$p0" -a "e0=$e0" -a "x0=$x0" -a "charge=$charge" -a "nwalkers=$nwalkers" -a "ntemps=$ntemps" -a "nsteps=$nsteps" -a "dev=$dev" -a "output_filename=$output_filename" submit_file.submit
+# # Execute the Python command and redirect output to the dynamic filename
+# condor_submit -a "Tobs=$Tobs" -a "dt=$dt" -a "M=$M" -a "mu=$mu" -a "a=$a" -a "p0=$p0" -a "e0=$e0" -a "x0=$x0" -a "charge=$charge" -a "nwalkers=$nwalkers" -a "ntemps=$ntemps" -a "nsteps=$nsteps" -a "dev=$dev" -a "output_filename=$output_filename" submit_file.submit
+
+# a=0.95
+# # ---------------------------------------------------------
+# dev=2
+# e0=0.2
+# # Create a dynamic output filename
+# output_filename="output_lakshmi_Tobs${Tobs}_dt${dt}_M${M}_mu${mu}_a${a}_p0${p0}_e0${e0}_x0${x0}_charge${charge}_dev${dev}_nwalkers${nwalkers}_ntemps${ntemps}_nsteps${nsteps}.txt"
+
+# # Execute the Python command and redirect output to the dynamic filename
+# condor_submit -a "Tobs=$Tobs" -a "dt=$dt" -a "M=$M" -a "mu=$mu" -a "a=$a" -a "p0=$p0" -a "e0=$e0" -a "x0=$x0" -a "charge=$charge" -a "nwalkers=$nwalkers" -a "ntemps=$ntemps" -a "nsteps=$nsteps" -a "dev=$dev" -a "output_filename=$output_filename" submit_file.submit
+
+# e0=0.4
+
+# # ---------------------------------------------------------
+# dev=3
+# M=5e5
+# # Create a dynamic output filename
+# output_filename="output_lakshmi_Tobs${Tobs}_dt${dt}_M${M}_mu${mu}_a${a}_p0${p0}_e0${e0}_x0${x0}_charge${charge}_dev${dev}_nwalkers${nwalkers}_ntemps${ntemps}_nsteps${nsteps}.txt"
+
+# # Execute the Python command and redirect output to the dynamic filename
+# condor_submit -a "Tobs=$Tobs" -a "dt=$dt" -a "M=$M" -a "mu=$mu" -a "a=$a" -a "p0=$p0" -a "e0=$e0" -a "x0=$x0" -a "charge=$charge" -a "nwalkers=$nwalkers" -a "ntemps=$ntemps" -a "nsteps=$nsteps" -a "dev=$dev" -a "output_filename=$output_filename" submit_file.submit
+
+# # ---------------------------------------------------------
+# dev=4
+# M=1e6
+# mu=5
+# # Create a dynamic output filename
+# output_filename="output_lakshmi_Tobs${Tobs}_dt${dt}_M${M}_mu${mu}_a${a}_p0${p0}_e0${e0}_x0${x0}_charge${charge}_dev${dev}_nwalkers${nwalkers}_ntemps${ntemps}_nsteps${nsteps}.txt"
+
+# # Execute the Python command and redirect output to the dynamic filename
+# condor_submit -a "Tobs=$Tobs" -a "dt=$dt" -a "M=$M" -a "mu=$mu" -a "a=$a" -a "p0=$p0" -a "e0=$e0" -a "x0=$x0" -a "charge=$charge" -a "nwalkers=$nwalkers" -a "ntemps=$ntemps" -a "nsteps=$nsteps" -a "dev=$dev" -a "output_filename=$output_filename" submit_file.submit
