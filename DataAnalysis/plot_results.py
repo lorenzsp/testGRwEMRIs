92cfa696a44e004e936e1d6ed92e9f55f57eab9a
Lorenzo Speri <EMAIL@DOMAIN.TLD>
2024-04-09 19:33:18 +0200

    new plots

--- DataAnalysis/plot_results.py
+++ DataAnalysis/plot_results.py
@@ -1,5 +1,4 @@
 import glob
-from eryn.backends import HDFBackend
 import numpy as np
 import matplotlib.pyplot as plt
 import corner
@@ -8,6 +7,7 @@ from few.utils.constants import *
 import matplotlib as mpl
 import re
 import matplotlib.style as style
+import seaborn as sns
 style.use('tableau-colorblind10')
 
 default_width = 5.78853 # in inches
@@ -46,11 +46,19 @@ mpl.rcParams.update({
 init_name = 'results_paper/mcmc_rndStart_M*_charge0.0_*seed2601*'
 datasets = sorted(glob.glob(init_name + '.h5'))
 pars_inj = sorted(glob.glob(init_name + '_injected_pars.npy'))
+# sort datasets by charge
+datasets = ['results_paper/mcmc_rndStart_M1e+06_mu1e+01_a0.8_p8.7_e0.4_x1.0_charge0.0_SNR50.0_T2.0_seed2601_nw16_nt1.h5', 'results_paper/mcmc_rndStart_M1e+06_mu1e+01_a0.95_p8.3_e0.4_x1.0_charge0.0_SNR50.0_T2.0_seed2601_nw16_nt1.h5', 'results_paper/mcmc_rndStart_M1e+06_mu1e+01_a0.95_p8.4_e0.1_x1.0_charge0.0_SNR50.0_T2.0_seed2601_nw16_nt1.h5', 'results_paper/mcmc_rndStart_M1e+06_mu5.0_a0.95_p6.9_e0.4_x1.0_charge0.0_SNR50.0_T2.0_seed2601_nw16_nt1.h5', 'results_paper/mcmc_rndStart_M5e+05_mu1e+01_a0.95_p1.2e+01_e0.4_x1.0_charge0.0_SNR50.0_T2.0_seed2601_nw16_nt1.h5', 'results_paper/mcmc_rndStart_M1e+06_mu1e+01_a0.95_p8.3_e0.4_x1.0_charge0.0025_SNR50.0_T2.0_seed2601_nw16_nt1.h5', ]
+# create the list of injected parameters
+pars_inj = ['results_paper/mcmc_rndStart_M1e+06_mu1e+01_a0.8_p8.7_e0.4_x1.0_charge0.0_SNR50.0_T2.0_seed2601_nw16_nt1_injected_pars.npy', 'results_paper/mcmc_rndStart_M1e+06_mu1e+01_a0.95_p8.3_e0.4_x1.0_charge0.0_SNR50.0_T2.0_seed2601_nw16_nt1_injected_pars.npy', 'results_paper/mcmc_rndStart_M1e+06_mu1e+01_a0.95_p8.4_e0.1_x1.0_charge0.0_SNR50.0_T2.0_seed2601_nw16_nt1_injected_pars.npy', 'results_paper/mcmc_rndStart_M1e+06_mu5.0_a0.95_p6.9_e0.4_x1.0_charge0.0_SNR50.0_T2.0_seed2601_nw16_nt1_injected_pars.npy', 'results_paper/mcmc_rndStart_M5e+05_mu1e+01_a0.95_p1.2e+01_e0.4_x1.0_charge0.0_SNR50.0_T2.0_seed2601_nw16_nt1_injected_pars.npy', 'results_paper/mcmc_rndStart_M1e+06_mu1e+01_a0.95_p8.3_e0.4_x1.0_charge0.0025_SNR50.0_T2.0_seed2601_nw16_nt1_injected_pars.npy', ]
+
 print("len names", len(datasets),len(pars_inj))
 cmap = plt.cm.get_cmap('Set1',)
 colors = ['#377eb8', '#ff7f00', '#4daf4a',
                   '#f781bf', '#a65628', '#984ea3',
                   '#999999', '#e41a1c', '#dede00']
+
+
+colors = sns.color_palette('colorblind')
 # colors = [cmap(i) for i in range(len(datasets))]#['black','red', 'royalblue']#
 ls = ['-','--','-.',':',(0, (2, 2)),(0, (1, 2))]
 # provide custom line styles
@@ -97,7 +105,7 @@ for filename,el,cc,ll in zip(datasets,pars_inj,colors,ls):
     y = np.sqrt(2*d)*mu*MRSUN_SI/1e3
     bins = np.linspace(-1.5,0.3,num=25)+ np.random.uniform(-0.05,-0.0001)
     
-    plt.hist(np.log10(y), weights=w/y, bins=bins, histtype='step', density=True, label=label, linewidth=3, ls=ll, color=cc)
+    plt.hist(np.log10(y), weights=w/y, bins=bins, histtype='step', density=True, label=label, linewidth=3, ls=ll)#, color=cc)
     # plt.axvline(np.quantile(np.log10(y),0.975),color=cc)
 
 plt.tight_layout()
@@ -130,6 +138,11 @@ from scipy.stats import gaussian_kde
 init_name = 'results_paper/mcmc_rndStart_M*_charge0.0*seed2601*'
 datasets = sorted(glob.glob(init_name + '.h5'))
 pars_inj = sorted(glob.glob(init_name + '_injected_pars.npy'))
+# sort datasets by charge
+datasets = ['results_paper/mcmc_rndStart_M1e+06_mu1e+01_a0.8_p8.7_e0.4_x1.0_charge0.0_SNR50.0_T2.0_seed2601_nw16_nt1.h5', 'results_paper/mcmc_rndStart_M1e+06_mu1e+01_a0.95_p8.3_e0.4_x1.0_charge0.0_SNR50.0_T2.0_seed2601_nw16_nt1.h5', 'results_paper/mcmc_rndStart_M1e+06_mu1e+01_a0.95_p8.4_e0.1_x1.0_charge0.0_SNR50.0_T2.0_seed2601_nw16_nt1.h5', 'results_paper/mcmc_rndStart_M1e+06_mu5.0_a0.95_p6.9_e0.4_x1.0_charge0.0_SNR50.0_T2.0_seed2601_nw16_nt1.h5', 'results_paper/mcmc_rndStart_M5e+05_mu1e+01_a0.95_p1.2e+01_e0.4_x1.0_charge0.0_SNR50.0_T2.0_seed2601_nw16_nt1.h5', 'results_paper/mcmc_rndStart_M1e+06_mu1e+01_a0.95_p8.3_e0.4_x1.0_charge0.0025_SNR50.0_T2.0_seed2601_nw16_nt1.h5', ]
+# create the list of injected parameters
+pars_inj = ['results_paper/mcmc_rndStart_M1e+06_mu1e+01_a0.8_p8.7_e0.4_x1.0_charge0.0_SNR50.0_T2.0_seed2601_nw16_nt1_injected_pars.npy', 'results_paper/mcmc_rndStart_M1e+06_mu1e+01_a0.95_p8.3_e0.4_x1.0_charge0.0_SNR50.0_T2.0_seed2601_nw16_nt1_injected_pars.npy', 'results_paper/mcmc_rndStart_M1e+06_mu1e+01_a0.95_p8.4_e0.1_x1.0_charge0.0_SNR50.0_T2.0_seed2601_nw16_nt1_injected_pars.npy', 'results_paper/mcmc_rndStart_M1e+06_mu5.0_a0.95_p6.9_e0.4_x1.0_charge0.0_SNR50.0_T2.0_seed2601_nw16_nt1_injected_pars.npy', 'results_paper/mcmc_rndStart_M5e+05_mu1e+01_a0.95_p1.2e+01_e0.4_x1.0_charge0.0_SNR50.0_T2.0_seed2601_nw16_nt1_injected_pars.npy', 'results_paper/mcmc_rndStart_M1e+06_mu1e+01_a0.95_p8.3_e0.4_x1.0_charge0.0025_SNR50.0_T2.0_seed2601_nw16_nt1_injected_pars.npy', ]
+
 print("len names", len(datasets),len(pars_inj))
 # cmap = plt.cm.get_cmap('Set1',)
 # colors = [cmap(i) for i in range(len(datasets))]#['black','red', 'royalblue']#
@@ -137,12 +150,6 @@ ls = ['-','--','-.',':',(0, (3, 1, 1, 1, 3))]
 # can you find a better way to plot the lines?
 #
 ls = ['-', '--', '-.', ':', (0, (3, 1, 1, 1, 3)), (0, (1, 3))]
-# ls = ['-', '--', '-.', ':', (0, (3, 1, 1, 1, 3)), (0, (3, 5, 1, 5, 1))]
-
-# sort datasets by charge
-datasets = ['results_paper/mcmc_rndStart_M1e+06_mu1e+01_a0.8_p8.7_e0.4_x1.0_charge0.0_SNR50.0_T2.0_seed2601_nw16_nt1.h5', 'results_paper/mcmc_rndStart_M1e+06_mu1e+01_a0.95_p8.3_e0.4_x1.0_charge0.0_SNR50.0_T2.0_seed2601_nw16_nt1.h5', 'results_paper/mcmc_rndStart_M1e+06_mu1e+01_a0.95_p8.4_e0.1_x1.0_charge0.0_SNR50.0_T2.0_seed2601_nw16_nt1.h5', 'results_paper/mcmc_rndStart_M1e+06_mu5.0_a0.95_p6.9_e0.4_x1.0_charge0.0_SNR50.0_T2.0_seed2601_nw16_nt1.h5', 'results_paper/mcmc_rndStart_M5e+05_mu1e+01_a0.95_p1.2e+01_e0.4_x1.0_charge0.0_SNR50.0_T2.0_seed2601_nw16_nt1.h5', 'results_paper/mcmc_rndStart_M1e+06_mu1e+01_a0.95_p8.3_e0.4_x1.0_charge0.0025_SNR50.0_T2.0_seed2601_nw16_nt1.h5', ]
-# create the list of injected parameters
-pars_inj = ['results_paper/mcmc_rndStart_M1e+06_mu1e+01_a0.8_p8.7_e0.4_x1.0_charge0.0_SNR50.0_T2.0_seed2601_nw16_nt1_injected_pars.npy', 'results_paper/mcmc_rndStart_M1e+06_mu1e+01_a0.95_p8.3_e0.4_x1.0_charge0.0_SNR50.0_T2.0_seed2601_nw16_nt1_injected_pars.npy', 'results_paper/mcmc_rndStart_M1e+06_mu1e+01_a0.95_p8.4_e0.1_x1.0_charge0.0_SNR50.0_T2.0_seed2601_nw16_nt1_injected_pars.npy', 'results_paper/mcmc_rndStart_M1e+06_mu5.0_a0.95_p6.9_e0.4_x1.0_charge0.0_SNR50.0_T2.0_seed2601_nw16_nt1_injected_pars.npy', 'results_paper/mcmc_rndStart_M5e+05_mu1e+01_a0.95_p1.2e+01_e0.4_x1.0_charge0.0_SNR50.0_T2.0_seed2601_nw16_nt1_injected_pars.npy', 'results_paper/mcmc_rndStart_M1e+06_mu1e+01_a0.95_p8.3_e0.4_x1.0_charge0.0025_SNR50.0_T2.0_seed2601_nw16_nt1_injected_pars.npy', ]
 
 # Scalar plot
 plt.figure()
@@ -194,7 +201,7 @@ for filename,el,cc,ll in zip(datasets,pars_inj,colors,ls):
     
     label += ')'
 
-    plt.hist(toplot[:,-1], bins=40, histtype='step', density=True, label=label, linewidth=3, ls=ll, color=cc)
+    plt.hist(toplot[:,-1], bins=40, histtype='step', density=True, label=label, linewidth=3, ls=ll)#, color=cc)
     # plot kde of the last parameter
     # plt.plot(np.linspace(toplot[:,-1].min(),toplot[:,-1].max(),100),kde.evaluate(np.linspace(toplot[:,-1].min(),toplot[:,-1].max(),100)),color=cc,linestyle=ll,label=label, linewidth=3,)
 
@@ -206,62 +213,6 @@ plt.legend(title='\t \t'+r'$(M \, [{\rm M}_\odot], \mu \, [{\rm M}_\odot], a, e_
 # plt.legend()
 plt.ylim(0.0,1970)
 plt.savefig(f'./plot_paper/charge_bound_hist.pdf', bbox_inches='tight')
-
-#####################################
-# following the code above could you create a violin plot where the x-axis is the label?
-# Violin plot
-var = -1
-plt.figure(figsize=(12, 8))
-data = []
-labels = []
-for filename, el in zip(datasets, pars_inj):
-    # get_repo name
-    repo_name = el.split('_injected_pars.npy')[0]
-    repo_name
-    truths = np.load(el)
-    toplot = np.load(repo_name + '/samples.npy')
-
-
-    # Get the parameter of interest
-    parameter = toplot[:, var]
-    # Add the parameter to the data list
-    data.append(parameter)
-     # Parse parameters from repo_name
-    params = repo_name.split('_')[3:]
-    params_dict = {}
-    
-    for param in params:
-        name_to_split = re.match(r'([a-zA-Z]+)', param).groups()[0]
-        key, value = name_to_split, float(param.split(name_to_split)[1])
-        params_dict[key] = value
-
-    # labels
-    label = '('
-    label += fr"{params_dict.get('M')/1e6}$\times 10^6$"
-    if int(params_dict.get('mu'))==5:
-        label += f", $\, \, \,${int(params_dict.get('mu'))}"
-    else:
-        label += f", {int(params_dict.get('mu'))}"
-    label += f", {params_dict.get('a'):.2f}"
-    label += f", {params_dict.get('e')}"
-    if truths[-1]!=0.0:
-        label += fr", $>${savage_dickey:.2f}"
-    else:
-        label += f", {savage_dickey:.2f}"
-        
-    label += ')'
-
-    # Add the label to the labels list
-    labels.append(label)
-# Create the violin plot
-plt.violinplot(data, showmedians=True)
-# Set the x-axis labels
-plt.xticks(range(1, len(labels) + 1), labels, rotation=45)
-plt.tight_layout()
-plt.xlabel(labels[var], size=22)
-plt.ticklabel_format(style='sci')
-plt.legend(title=r'$(M \, [{\rm M}_\odot], \mu \, [{\rm M}_\odot], a, e_0)$')
-plt.savefig(f'./plot_paper/variable_{var}.pdf', bbox_inches='tight')
 
 #####################################
 labels = [r'$\Delta \ln (M/{\rm M}_\odot$)', r'$\Delta \ln (\mu / M_{\odot})$', r'$\Delta a$', r'$\Delta p_0 \, [M]$', r'$\Delta e_0$', 
