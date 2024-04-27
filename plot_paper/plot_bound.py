73d3b446b750ba6105f6c662cb40c6a2e7e2442e
Lorenzo Speri <lorenzo.speri@gmail.com>
2024-04-27 15:48:58 +0200

    new figures

--- plot_paper/plot_bound.py
+++ plot_paper/plot_bound.py
@@ -115,33 +115,55 @@ for filename,el,cc,ll in zip(datasets,pars_inj,colors,ls):
 
 plt.tight_layout()
 plt.xlabel(r'$\log_{10} [\sqrt{\alpha} / {\rm km} ]$',size=22)
-vpos = 0.8
 plt.ticklabel_format(style='sci')
-# 
-# plt.axvline(vpos,color='k',linestyle=':',label='Current bound', linewidth=2)
-plt.annotate('Current bound \nfrom LVK', xy=(vpos, 0.0), xytext=(vpos, 0.35),
-             arrowprops=dict(facecolor='black', shrink=0.001),
-             fontsize=12, ha='center')
 
-vpos = np.log10(0.4 * np.sqrt( 16 * np.pi**0.5 ))
-# plt.axvline(vpos,color='r',linestyle='-.',label='Best bound from 3G', linewidth=2)
+# vpos = 0.8
+# plt.annotate('Current bound \nfrom LVK', xy=(vpos, 0.0), xytext=(vpos, 0.35),
+#              arrowprops=dict(facecolor='black', shrink=0.001),
+#              fontsize=12, ha='center')
 
-# Add arrow with text
-plt.annotate('Best bound \nfrom 3G', xy=(vpos, 0.0), xytext=(vpos, 0.35),
-             arrowprops=dict(facecolor='black', shrink=0.001),
-             fontsize=12, ha='center')
+# vpos = np.log10(0.4 * np.sqrt( 16 * np.pi**0.5 ))
+# plt.annotate('Forecasted bound \nfrom 3G', xy=(vpos, 0.0), xytext=(vpos, 0.35),
+#              arrowprops=dict(facecolor='black', shrink=0.001),
+#              fontsize=12, ha='center')
 
-plt.legend(title=r'$(M \, [{\rm M}_\odot], \mu \, [{\rm M}_\odot], a, e_0)$')
+
+vpos = 0.8
+xlow = -1.5
+# Create a bar
+ylev = 1.3
+plt.broken_barh([(xlow, vpos-xlow)], (ylev,0.1), edgecolor='black')#, facecolors='none')
+plt.text(vpos+0.1, ylev+0.05, 'Current\nconstraint\nfrom LVK', ha='left', va='center', fontsize=12)
+
+Nsource_3g = 1e6
+vpos = np.log10(5e-2 * np.sqrt(16*np.pi**0.5))
+ylev = 1.5
+plt.broken_barh([(xlow, vpos-xlow)], (ylev,0.1), edgecolor='black')#, facecolors='none')
+plt.text(vpos+0.1, ylev+0.05, 'Projected cumulative constraint\nfrom LIGO Voyager 08/09', ha='left', va='center', fontsize=12)
+
+plt.legend(title=r'$(M \, [{\rm M}_\odot], \mu \, [{\rm M}_\odot], a, e_0)$',loc='lower right')
+
+# plt.legend(title=r'$(M \, [{\rm M}_\odot], \mu \, [{\rm M}_\odot], a, e_0)$')
 # plt.legend()
-plt.xlim(-1.5,1.1)
-plt.ylim(0.0,1.3)
+plt.xlim(xlow,1.3)
+plt.ylim(0.0,1.7)
 plt.savefig(f'./figures/bound_alpha.pdf', bbox_inches='tight')
 
 ########################################################################
 from few.trajectory.inspiral import EMRIInspiral
 from few.utils.utility import get_kerr_geo_constants_of_motion
 # get Edot function
-trajELQ = EMRIInspiral(func="KerrEccentricEquatorial")
+# trajELQ = EMRIInspiral(func="KerrEccentricEquatorial")
+trajELQ = EMRIInspiral(func="KerrEccentricEquatorialAPEX")
+# from few.summation.interpolatedmodesum import CubicSplineInterpolant
+#import cubic spline from scipy
+from scipy.interpolate import CubicSpline
+
+def get_dphi(M, mu, a, p0, e0, x0, charge):
+    t, p, e, x, Phi_phi, Phi_theta, Phi_r = trajELQ(M, mu, a, p0, e0, x0, charge, T=2.0, dt=10.0)
+    t_0, p, e, x, Phi_phi_0, Phi_theta, Phi_r = trajELQ(M, mu, a, p0, e0, x0, 0.0, T=2.0, dt=10.0)
+    interp = CubicSpline(t_0, Phi_phi_0)
+    return np.max((1-Phi_phi/interp(t))[1:-1])
 
 def get_Edot(M, mu, a, p0, e0, x0, charge):
     trajELQ.inspiral_generator.moves_check = 0
@@ -163,23 +185,26 @@ def get_Edot(M, mu, a, p0, e0, x0, charge):
     # define fixed variables
     trajELQ.inspiral_generator.integrator.add_parameters_to_holder(M, mu, a, np.asarray([charge]))
     y1, y2, y3 = get_kerr_geo_constants_of_motion(a, p0, e0, x0)
-    y0 = np.array([y1, y2, y3])
+    # y0 = np.array([y1, y2, y3])
+    y0 = np.array([p0, e0, x0, 0.0, 0.0, 0.0])
     return trajELQ.inspiral_generator.integrator.get_derivatives(y0)[0] / (mu/M)
 
 def get_delta_Edot(M, mu, a, p0, e0, x0, charge):
     # for defining B https://arxiv.org/pdf/1603.04075 and delta https://arxiv.org/pdf/1007.1995
     # https://arxiv.org/pdf/1007.1995
+    # paper for pulsar constraints in ppE: https://arxiv.org/pdf/2002.02030
     
     Edot_Charge = get_Edot(M, mu, a, p0, e0, x0, charge)
     Edot_ZeroCharge = get_Edot(M, mu, a, p0, e0, x0, 0.0)
     delta_Edot = Edot_Charge - Edot_ZeroCharge
     B = (delta_Edot/Edot_ZeroCharge) / p0
     delta = delta_Edot/Edot_ZeroCharge
-    return delta
+    return delta, B
 
 
+Nsamp = int(1e5)
 
-# Alpha plot
+#------------------------- delta plot ---------------------------------
 plt.figure()
 for filename,el,cc,ll in zip(datasets,pars_inj,colors,ls):
     # get_repo name
@@ -217,8 +242,7 @@ for filename,el,cc,ll in zip(datasets,pars_inj,colors,ls):
     newvar[:,5] = np.ones_like(newvar[:,6])
     newvar[:,6] = np.abs(toplot[:,-1] - np.median(toplot[:,-1])+ truths[-1])
     
-    Nsamp = int(1e5)
-    y = np.abs(np.asarray([get_delta_Edot(*newvar[ii]) for ii in range(Nsamp)]))
+    y = np.abs(np.asarray([get_delta_Edot(*newvar[ii])[0] for ii in range(Nsamp)]))
     bins = np.linspace(-11.5,-6.0,num=30) #+ np.random.uniform(-0.05,-0.0001)
     mask = (y!=0.0)
     plt.hist(np.log10(y[mask]),weights=1/newvar[:Nsamp,6][mask], bins=bins, histtype='step', density=True, label=label, linewidth=3, ls=ll)#, color=cc)
@@ -228,38 +252,157 @@ for filename,el,cc,ll in zip(datasets,pars_inj,colors,ls):
 plt.tight_layout()
 plt.xlabel(r'$\log_{10} \delta $',size=22)# {\dot{E}}
 vpos = np.log10(6e-4) # binary J1141-6545 from https://arxiv.org/pdf/1603.04075
+# approximately 60 000 orbits in  https://journals.aps.org/prx/pdf/10.1103/PhysRevX.11.041050
 vpos = np.log10(1-0.999963) # binary PSR J0737–3039 from https://journals.aps.org/prx/pdf/10.1103/PhysRevX.11.041050
 plt.ticklabel_format(style='sci')
-# # 
-# plt.axvline(vpos,color='k',linestyle=':',label='Current bound', linewidth=2)
 plt.annotate('Bound from binary \npulsar J1141-6545', xy=(vpos, 0.0), xytext=(vpos, 0.1),
              arrowprops=dict(facecolor='black', shrink=0.001),
              fontsize=12, ha='center')
-
-# approximately 60 000 orbits in  https://journals.aps.org/prx/pdf/10.1103/PhysRevX.11.041050
-
-# from fig 12 https://arxiv.org/pdf/2010.09010
-vpos = np.log10(3e-6)
-# plt.axvline(vpos,color='r',linestyle='-.',label='Best bound from 3G', linewidth=2)
-
-# Add arrow with text
-# plt.annotate('Best bound \nfrom 3G', xy=(vpos, 0.0), xytext=(vpos, 0.35),
-#              arrowprops=dict(facecolor='black', shrink=0.001),
-#              fontsize=12, ha='center')
 
 plt.legend(title=r'$(M \, [{\rm M}_\odot], \mu \, [{\rm M}_\odot], a, e_0)$')
 # plt.legend()
 plt.xlim(-11.0,-2.0)
 # plt.ylim(0.0,1.3)
-plt.savefig(f'./figures/bound_deltaEdot.pdf', bbox_inches='tight')
+plt.savefig(f'./figures/bound_delta.pdf', bbox_inches='tight')
+
 # estimate period of the binary using the fundamental frequencies
-from few.utils.utility import get_fundamental_frequencies
-MTSUN_SI
-M,mu,a,p0,e0,x0=np.median(newvar,axis=0)[:6]
-frequency = get_fundamental_frequencies(a,p0,e0,x0)[0] / (2*np.pi*M*MTSUN_SI)
-period = 1/frequency
-breakpoint()
-########################################################################
+# from few.utils.utility import get_fundamental_frequencies
+# MTSUN_SI
+# M,mu,a,p0,e0,x0=np.median(newvar,axis=0)[:6]
+# frequency = get_fundamental_frequencies(a,p0,e0,x0)[0] / (2*np.pi*M*MTSUN_SI)
+# period = 1/frequency
+# breakpoint()
+#------------------------- delta Edot ---------------------------------
+plt.figure()
+for filename,el,cc,ll in zip(datasets,pars_inj,colors,ls):
+    # get_repo name
+    repo_name = el.split('_injected_pars.npy')[0]
+    repo_name
+    truths = np.load(el)
+    toplot = np.load(repo_name + '/samples.npy')
+    
+    # Parse parameters from repo_name
+    params = repo_name.split('_')[3:]
+    params_dict = {}
+    
+    for param in params:
+        name_to_split = re.match(r'([a-zA-Z]+)', param).groups()[0]
+        key, value = name_to_split, float(param.split(name_to_split)[1])
+        params_dict[key] = value
+
+    # labels
+    label = '('
+
+    # label += f"{params_dict.get('T')}"
+    label += fr"{params_dict.get('M')/1e6}$\times 10^6$"
+    if int(params_dict.get('mu'))==5:
+        label += f", $\, \, \,${int(params_dict.get('mu'))}"
+    else:
+        label += f", {int(params_dict.get('mu'))}"
+    label += f", {params_dict.get('a'):.2f}"
+    label += f", {params_dict.get('e')}"
+    label += ')'
+
+    
+    # alpha bound
+    newvar = toplot[:,:7].copy() - np.median(toplot[:,:7],axis=0) + truths[:7]
+    newvar[:,0],newvar[:,1] = np.exp(newvar[:,0]), np.exp(newvar[:,1])
+    newvar[:,5] = np.ones_like(newvar[:,6])
+    newvar[:,6] = np.abs(toplot[:,-1] - np.median(toplot[:,-1])+ truths[-1])
+    
+    y = np.abs(np.asarray([get_delta_Edot(*newvar[ii])[1] for ii in range(Nsamp)]))
+    bins = np.linspace(-11.5,-6.0,num=30) #+ np.random.uniform(-0.05,-0.0001)
+    mask = (y!=0.0)
+    plt.hist(np.log10(y[mask]),weights=1/newvar[:Nsamp,6][mask], bins=bins, histtype='step', density=True, label=label, linewidth=3, ls=ll)#, color=cc)
+    # 
+    # plt.axvline(np.quantile(np.log10(y),0.975),color=cc)
+
+plt.tight_layout()
+plt.xlabel(r'$\log_{10} \delta \dot{E} $',size=22)# {\dot{E}}
+# from fig 11 https://arxiv.org/pdf/2010.09010
+vpos = np.log10(1e-8)
+# plt.annotate('Projected\ncumulative\nconstraint\n3G', xy=(vpos, -0.25), xytext=(vpos, -0.25),
+#              arrowprops=dict(facecolor='black', shrink=0.001),
+#              fontsize=12, ha='center',
+#              xycoords='data', annotation_clip=False
+#              )
+
+xlow = -11.0
+# Create a bar
+plt.broken_barh([(xlow, vpos-xlow)], (0.65,0.1), edgecolor='black')#, facecolors='none')
+plt.text(vpos+0.1, 0.7, 'SOBHs observed by \nfuture ground-based detectors', ha='left', va='center', fontsize=12)
+# add the broken bar for the next valur of vpos
+
+vpos = np.log10(1e-10)
+plt.broken_barh([(xlow, vpos-xlow)], (0.8,0.1), edgecolor='black')#, facecolors='none')
+plt.text(vpos+0.1, 0.85, 'SOBHs observed by \nLISA + future ground-based detectors (multiband)', ha='left', va='center', fontsize=12)
+
+
+plt.legend(title=r'$(M \, [{\rm M}_\odot], \mu \, [{\rm M}_\odot], a, e_0)$',loc='lower right')
+# plt.legend()
+plt.xlim(-11.0,-4.0)
+plt.ylim(0.0,0.95)
+plt.savefig(f'./figures/bound_deltaEdot.pdf', bbox_inches='tight')
+# breakpoint()
+#----------------------------- delta phi ------------------------------------
+# plt.figure()
+# for filename,el,cc,ll in zip(datasets,pars_inj,colors,ls):
+#     # get_repo name
+#     repo_name = el.split('_injected_pars.npy')[0]
+#     repo_name
+#     truths = np.load(el)
+#     toplot = np.load(repo_name + '/samples.npy')
+    
+#     # Parse parameters from repo_name
+#     params = repo_name.split('_')[3:]
+#     params_dict = {}
+    
+#     for param in params:
+#         name_to_split = re.match(r'([a-zA-Z]+)', param).groups()[0]
+#         key, value = name_to_split, float(param.split(name_to_split)[1])
+#         params_dict[key] = value
+
+#     # labels
+#     label = '('
+
+#     # label += f"{params_dict.get('T')}"
+#     label += fr"{params_dict.get('M')/1e6}$\times 10^6$"
+#     if int(params_dict.get('mu'))==5:
+#         label += f", $\, \, \,${int(params_dict.get('mu'))}"
+#     else:
+#         label += f", {int(params_dict.get('mu'))}"
+#     label += f", {params_dict.get('a'):.2f}"
+#     label += f", {params_dict.get('e')}"
+#     label += ')'
+
+    
+#     # alpha bound
+#     newvar = toplot[:,:7].copy() - np.median(toplot[:,:7],axis=0) + truths[:7]
+#     newvar[:,0],newvar[:,1] = np.exp(newvar[:,0]), np.exp(newvar[:,1])
+#     newvar[:,5] = np.ones_like(newvar[:,6])
+#     newvar[:,6] = np.abs(toplot[:,-1] - np.median(toplot[:,-1])+ truths[-1])
+    
+#     y = np.abs(np.asarray([get_dphi(*newvar[ii]) for ii in range(Nsamp)]))
+#     bins = np.linspace(-11.5,-6.0,num=30) #+ np.random.uniform(-0.05,-0.0001)
+#     mask = (y!=0.0)
+#     plt.hist(np.log10(y[mask]),weights=1/newvar[:Nsamp,6][mask], bins=bins, histtype='step', density=True, label=label, linewidth=3, ls=ll)#, color=cc)
+#     # 
+#     # plt.axvline(np.quantile(np.log10(y),0.975),color=cc)
+
+# plt.tight_layout()
+# plt.xlabel(r'$\log_{10} \delta \phi $',size=22)# {\dot{E}}
+# vpos = np.log10(1e-7) # bound from https://arxiv.org/pdf/2002.02030 table IV
+# plt.ticklabel_format(style='sci')
+# # # 
+# plt.annotate('Bound from LVK and binary pulsars', xy=(vpos, 0.0), xytext=(vpos, 0.1),
+#              arrowprops=dict(facecolor='black', shrink=0.001),
+#              fontsize=12, ha='center')
+
+# plt.legend(title=r'$(M \, [{\rm M}_\odot], \mu \, [{\rm M}_\odot], a, e_0)$')
+# # plt.legend()
+# plt.xlim(-11.0,-6.0)
+# # plt.ylim(0.0,1.3)
+# plt.savefig(f'./figures/bound_deltaphi.pdf', bbox_inches='tight')
 
 ########################################################################
 from scipy.stats import gaussian_kde
