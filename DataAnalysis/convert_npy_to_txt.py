47ce8c547f32964128bd6a138dd20761bbd2f0c7
Lorenzo <lorenzo.speri@gmail.com>
2024-05-09 17:16:29 +0200

    update convert and delete samples.npy

--- DataAnalysis/convert_npy_to_txt.py
+++ DataAnalysis/convert_npy_to_txt.py
@@ -1,24 +1,62 @@
 import glob
 import numpy as np
 import subprocess
+from few.utils.constants import *
+import zipfile
+import os
 
 # Get a list of all samples.npy files in the results/*/*.npy directories
-npy_files = glob.glob('results/*/*.npy')
+npy_files = glob.glob('paper_runs/*/*.npy')
 
+list_txt = []
 # Convert each file to a .txt file
 for npy_file in npy_files:
     # Load the numpy array
     array = np.load(npy_file)
 
-    # Convert the array to float32
-    # array = array.astype('float32')
+    # Select samples
+    Lambda = array[:,-1]
+    array = array[Lambda>0.0]
+    Lambda = array[:,-1]
+    mu = np.exp(array[:,1])
+    
+    sqrt_alpha = 2*np.sqrt(2)*mu*MRSUN_SI/1e3*Lambda**(1/4)
+    weights = mu * Lambda**(-3/4) * np.sqrt(2)/4
+    
+    charge = np.sqrt(4 * Lambda)
+    weights_ch=1/np.sqrt(Lambda)
+    
+    headers = ' '.join([
+    'ln M',
+    'ln mu',
+    'a',
+    'p0',
+    'e0',
+    'DL',
+    'costhetaS',
+    'phiS',
+    'costhetaK',
+    'phiK',
+    'Phivarphi0',
+    'Phir0',
+    'Lambda',
+    'charge',
+    'charge_weights',
+    'sqrt_alpha',
+    'sqrt_alpha_weights'
+    ])
+
+    array_to_save = np.hstack((array,charge[:,None],weights_ch[:,None],sqrt_alpha[:,None],weights[:,None]))
 
     # Write the array to a .txt file
-    np.savetxt(npy_file.replace('.npy', '.txt'), array)
+    np.savetxt(npy_file.replace('.npy', '.txt'), array_to_save, header=headers)
     
-    # Run the zip command
-    subprocess.run(['zip', '-r', npy_file.replace('.npy', '.zip'), npy_file.replace('.npy', '.txt')])
+    list_txt += [npy_file.replace('.npy', '.txt')]
     
+
+# Run the zip command
+subprocess.run(['zip', '-r', 'samples.zip', *list_txt])
+
+for npy_file in npy_files:
     # now remove the .txt file
     subprocess.run(['rm', npy_file.replace('.npy', '.txt')])
-
